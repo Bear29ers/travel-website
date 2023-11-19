9cc0edaad22b44c1ce4f0157fde96a3999ede57c
Yuta Okuma <y.dream.nest@icloud.com>
2023-11-19 21:00:42 +0900

    :sparkles: add store buttons

--- components/Features.tsx
+++ components/Features.tsx
@@ -24,7 +24,7 @@ const FeatureItem: FC<FeatureItem> = ({ title, icon, description }) => {
 
 const Features: FC = () => {
   return (
-    <section className="flexCenter flex-col overflow-hidden border-2 border-red-500 bg-feature-bg bg-no-repeat py-24">
+    <section className="flexCenter flex-col overflow-hidden bg-feature-bg bg-no-repeat py-24">
       <div className="max-container padding-container relative flex w-full justify-end">
         <div className="flex flex-1 lg:min-h-[900px]">
           <Image src="/phone.png" alt="phone" width={440} height={1000} className="feature-phone" />
